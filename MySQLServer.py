5e694f7a14794cae7afcf2526425d66ec4259451
root <fissaa.amin@gmail.com>
2025-02-10 07:50:30 -0800

    Add Python script to create alx_book_store database 3

--- MySQLServer.py
+++ MySQLServer.py
@@ -16,8 +16,8 @@ def create_database():
             cursor.execute("CREATE DATABASE IF NOT EXISTS alx_book_store")
             print("Database 'alx_book_store' created successfully!")
 
-    except Error as e:
-        print(f"except {e}")
+    except mysql.connector.Error as e:  # Catch only MySQL errors
+        print(f"Error connecting to MySQL: {e}")
 
     finally:
         if 'cursor' in locals():
